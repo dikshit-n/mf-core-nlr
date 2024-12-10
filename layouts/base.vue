540a142cdc49b52cdefd5821da4328a81e7fa082
dikshit-n <dikshitkumarn@gmail.com>
2024-12-10 21:17:20 +0530

    base layout

--- layouts/base.vue
+++ layouts/base.vue
@@ -1,5 +1,24 @@
 <template>
   <header>Base Layout</header>
-  <main><slot/></main>
+  <div class="content">
+    <div class="sidebar">
+      <h1>HealPros</h1>
+      <ul>
+        <li v-for="route in routes" :key="route.name">
+          <NuxtLink :to="route.to">
+            {{ route.name }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <main><slot/></main>
+  </div>
   <footer>Footer</footer>
 </template>
+
+<script setup lang="ts">
+import { withDefaults, defineProps } from 'vue';
+  withDefaults(defineProps<{ routes: { name: string, to: string }[] }>(), {
+    routes: [] as any
+  })
+</script>
