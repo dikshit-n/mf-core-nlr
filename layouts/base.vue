0aba6eec0863767d99c23c82c49fd4300232bdfe
dikshit-n <dikshitkumarn@gmail.com>
2024-12-12 22:57:59 +0530

    uninstall uuid

--- layouts/base.vue
+++ layouts/base.vue
@@ -32,8 +32,8 @@
 
 <script setup lang="ts">
 import { withDefaults, defineProps } from 'vue';
-import { v4 } from 'uuid';
-console.log(v4(), '----uuid form mf-core')
+// import { v4 } from 'uuid';
+// console.log(v4(), '----uuid form mf-core')
   withDefaults(defineProps<{ routes: { name: string, to: string }[] }>(), {
     routes: [
       { name: 'Home', to: '/' },
