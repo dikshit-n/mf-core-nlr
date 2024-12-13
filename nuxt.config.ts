ed4da881db195d3d004599d472c9ae2c945207ca
dikshit-n <dikshitkumarn@gmail.com>
2024-12-13 23:21:20 +0530

    css file name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     // https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases
-    join(currentDir, './assets/main.css')
+    join(currentDir, './assets/base.css')
   ],
   modules: ['@nuxt/image']
 });
