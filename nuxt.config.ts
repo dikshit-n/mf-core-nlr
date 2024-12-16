a722359a6c44e6180f568a4591fd45402cfb7fec
dikshit-n <dikshitkumarn@gmail.com>
2024-12-16 16:21:08 +0530

    move css to public folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: [
-    '@/assets/base.css'
+    '/css/base.css'
   ],
   modules: ['@nuxt/image']
 });
