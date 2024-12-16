7f135d869f9333e3fb603cfe22816d36ff5ec0ae
dikshit-n <dikshitkumarn@gmail.com>
2024-12-16 15:15:12 +0530

    remove css config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: [
-    '@/assets/base.css'
-  ],
+  // css: [
+  //   '@/assets/base.css'
+  // ],
   modules: ['@nuxt/image']
 });
