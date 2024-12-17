03a9c8e5be94165d7223e4f44e044900637c1987
dikshit-n <dikshitkumarn@gmail.com>
2024-12-17 18:31:19 +0530

    content module

--- app.vue
+++ app.vue
@@ -1,43 +1,8 @@
 <template>
-  <NuxtLayout :routes="routes">
+  <NuxtLayout>
     <template #headerActions>
       Core App
     </template>
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script setup lang="ts">
-const routes = [
-  {
-    label: "Dashboard",
-    path: "/dashboard",
-    icon: "pi pi-palette",
-    roles: ["admin", "user"]
-  },
-  {
-    label: "Orders",
-    path: "/orders",
-    icon: "pi pi-home",
-    roles: ["admin"]
-  },
-  {
-    label: "Administration",
-    icon: "admin-icon",
-    roles: ["admin"],
-    submenus: [
-      {
-        label: "Accounts",
-        path: "/admin/accounts",
-        roles: ["admin"],
-        icon: 'pi pi-bookmark'
-      },
-      {
-        label: "Services",
-        path: "/admin/services",
-        roles: ["admin"],
-        icon: 'pi pi-bookmark'
-      }
-    ]
-  }
-]
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,7 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: [
-    '@nuxt/image',
-    '@primevue/nuxt-module'
-  ],
+  modules: ['@nuxt/image', '@primevue/nuxt-module', '@nuxt/content'],
   primevue: {
     autoImport: true,
     options: {
