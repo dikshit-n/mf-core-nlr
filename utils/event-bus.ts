8b6724a7e2513e7b2d283358d048a2c3e0cb1dfc
janani-irseponsive <janani.k@iresponsivesolutions.com>
2024-12-17 20:37:00 +0530

    eventbus added

--- utils/event-bus.ts
+++ utils/event-bus.ts
@@ -1,37 +1,34 @@
 // // core/utils/eventbus.ts
-// import { Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 
-// export class EventBus {
-//   private events = new Map<string, Subject<any>>();
+export class EventBus {
+  private events = new Map<string, Subject<any>>();
 
-//   // Emit an event with a specific key
-//   emit(eventKey: string, payload: any): void {
-//     if (!this.events.has(eventKey)) {
-//       this.events.set(eventKey, new Subject<any>());
-//     }
-//     this.events.get(eventKey)!.next(payload);
-//   }
+  // Emit an event with a specific key
+  emit(eventKey: string, payload: any): void {
+    if (!this.events.has(eventKey)) {
+      this.events.set(eventKey, new Subject<any>());
+    }
+    this.events.get(eventKey)!.next(payload);
+  }
 
-//   // Listen to an event with a specific key
-//   on(eventKey: string, callback: (data: any) => void): void {
-//     if (!this.events.has(eventKey)) {
-//       this.events.set(eventKey, new Subject<any>());
-//     }
-//     this.events.get(eventKey)!.subscribe(callback);
-//   }
+  // Listen to an event with a specific key
+  on(eventKey: string, callback: (data: any) => void): void {
+    if (!this.events.has(eventKey)) {
+      this.events.set(eventKey, new Subject<any>());
+    }
+    this.events.get(eventKey)!.subscribe(callback);
+  }
 
-//   // Optional: clear all listeners for an event
-//   clear(eventKey: string): void {
-//     if (this.events.has(eventKey)) {
-//       this.events.get(eventKey)!.complete();
-//       this.events.delete(eventKey);
-//     }
-//   }
-// }
+  // Optional: clear all listeners for an event
+  clear(eventKey: string): void {
+    if (this.events.has(eventKey)) {
+      this.events.get(eventKey)!.complete();
+      this.events.delete(eventKey);
+    }
+  }
+}
 
-// // Export a singleton instance of EventBus
-// const eventBus = new EventBus();
-// export default eventBus;
-
-// undoing for testing
-export {};
+// Export a singleton instance of EventBus
+const eventBus = new EventBus();
+export default eventBus;
