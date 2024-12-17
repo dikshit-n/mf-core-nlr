448517eba5ea541c16690717a1f84e2a58547875
dikshit-n <dikshitkumarn@gmail.com>
2024-12-17 19:45:06 +0530

    rename type declaration file

---
+++ types/route.ts
@@ -0,0 +1,14 @@
+export interface Route {
+  label: string
+  path?: string
+  icon?: string
+  roles: string[]
+  submenus?: ChildRoute[]
+}
+
+export interface ChildRoute {
+  label: string
+  path?: string
+  icon: string
+  roles: string[]
+}
