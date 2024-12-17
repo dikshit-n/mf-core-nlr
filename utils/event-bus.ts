409fc72c6e0c86af1681304b76ed8700f4d75381
janani-irseponsive <janani.k@iresponsivesolutions.com>
2024-12-17 22:41:04 +0530

    broadcast added

--- utils/event-bus.ts
+++ utils/event-bus.ts
@@ -1,31 +1,28 @@
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, filter } from "rxjs";
 
 const broadcastChannel = new BroadcastChannel("microfrontend-events");
 
-// RxJS Event Bus
 class EventBus {
   private event$ = new BehaviorSubject<any>(null);
 
   emit(event: string, payload: any) {
     const data = { event, payload };
     this.event$.next(data);
-    // Send event via BroadcastChannel for cross-tab communication
     broadcastChannel.postMessage(data);
   }
 
   on(event: string, callback: (payload: any) => void) {
-    // Listen to events internally via RxJS
-    this.event$.subscribe((data) => {
-      if (data && data.event === event) {
-        callback(data.payload);
-      }
-    });
-    // Listen to events via BroadcastChannel
-    broadcastChannel.onmessage = (message) => {
-      if (message.data.event === event) {
+    // Filter out null emissions and match event names
+    this.event$
+      .pipe(filter((data) => data && data.event === event))
+      .subscribe((data) => callback(data.payload));
+
+    // Handle BroadcastChannel messages without overwriting existing handlers
+    broadcastChannel.addEventListener("message", (message) => {
+      if (message.data && message.data.event === event) {
         callback(message.data.payload);
       }
-    };
+    });
   }
 }
 
